82f1bf71c590b8e3892841eaa926cd5c427ce1f2
Nooowkie <alirahmanzanov2323@gmail.com>
2025-07-10 17:07:31 +0600

    HomeWork_2

--- project/js/home_works.js
+++ project/js/home_works.js
@@ -1,8 +1,8 @@
+// проверка gmail
 document.getElementById("gmail_button").addEventListener("click", function () {
   const gmailInput = document.getElementById("gmail_input").value.trim();
   const resultSpan = document.getElementById("gmail_result");
 
-  // проверка gmail
   const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
 
   if (gmailRegex.test(gmailInput)) {
@@ -15,15 +15,64 @@ document.getElementById("gmail_button").addEventListener("click", function () {
 });
 
 //Движение блока
-const childBlock = document.querySelector('.child_block')
-const parentBlock = document.querySelector('.parent_block')
-let positionX = 0
+const childBlock = document.querySelector('.child_block');
+const parentBlock = document.querySelector('.parent_block');
+
+let positionX = 0;
+let positionY = 0;
+
+const offsetWidth = parentBlock.clientWidth - childBlock.clientWidth;
+const offsetHeight = parentBlock.clientHeight - childBlock.clientHeight;
 
 const moveBlock = () => {
-    if(positionX < 450){
-        positionX++
+  if (positionX < offsetWidth && positionY === 0) {
+    positionX++;
+  } else if (positionX === offsetWidth && positionY < offsetHeight) {
+    positionY++;
+  } else if (positionY === offsetHeight && positionX > 0) {
+    positionX--;
+  } else if (positionX === 0 && positionY > 0) {
+    positionY--;
+  } else {
+    return;
+  }
+  
+  childBlock.style.left = `${positionX}px`;
+  childBlock.style.top = `${positionY}px`;
+
+  requestAnimationFrame(moveBlock);
+};
+
+moveBlock();
+
+//Счетчик
+  let seconds = 0;
+  let intervalId = null;
+
+  const secondsDisplay = document.getElementById('seconds');
+  const startBtn = document.getElementById('start');
+  const stopBtn = document.getElementById('stop');
+  const resetBtn = document.getElementById('reset');
+
+  startBtn.addEventListener('click', () => {
+    if (intervalId === null) {
+      intervalId = setInterval(() => {
+        seconds++;
+        secondsDisplay.textContent = seconds;
+      }, 500);
     }
-    childBlock.style.left = `${positionX}px`
-    requestAnimationFrame(moveBlock)
-}
-moveBlock()
+  });
+
+  stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+    intervalId = null;
+  });
+
+  resetBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+    intervalId = null;
+    setTimeout(() => {
+      seconds = 0;
+      secondsDisplay.textContent = seconds;
+    }, 300);
+  });
