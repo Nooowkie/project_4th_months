eea6f0bb0ed9f5a9092e2393b1b0c5ca1b583a98
Nooowkie <alirahmanzanov2323@gmail.com>
2025-07-21 18:47:02 +0600

    HM_5

--- project/js/lesson.js
+++ project/js/lesson.js
@@ -41,3 +41,46 @@ tabParent.onclick = (event) => {
 hideSlide()
 showSlide(index)
 autoSlider(index)
+
+//convertor
+
+const somInput = document.querySelector("#som")
+const usdInput = document.querySelector("#usd")
+const eurInput = document.querySelector("#eur")
+
+const convertor = (element) => {
+    element.oninput = () =>{
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', '../data/convertor.json')
+        xhr.setRequestHeader('Content-type', 'application/json')
+        xhr.send()
+
+        xhr.onload = () =>{
+            const data = JSON.parse(xhr.response)
+            if(element.id === 'som'){
+                usdInput.value = (element.value / data.usd).toFixed(2)
+                eurInput.value = (element.value / data.eur).toFixed(2)
+
+            }
+            if(element.id === 'usd'){
+                somInput.value = (element.value * data.usd).toFixed(2)
+                eurInput.value = (element.value * data.usd / data.eur).toFixed(2)
+            }
+            if(element.id === 'eur'){
+                usdInput.value = (element.value * data.eur / data.usd).toFixed(2)
+                somInput.value = (element.value * data.eur).toFixed(2)
+            }
+            if(element.value === ''){
+                somInput = ''
+                usdInput = ''
+                eurInput = ''
+            }
+
+        }
+    }
+}
+
+convertor(somInput)
+convertor(usdInput)
+convertor(eurInput)
+
